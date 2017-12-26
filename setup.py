d1c73c31681fd3281558204d2826b280ffe42ecd
crohaco <crohaco@beproud.jp>
2017-12-26 12:40:35 +0900

    setup.pyを追加

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python',
+    'Topic :: Software Development',
+    'Programming Language :: SQL',
+]
+keywords = [
+    'sql',
+]
+
+setup(
+    name='sql-importer',
+    version='0.0.0',
+    description='it enables sql files to be imported as a python module.',
+    long_description=open('./README.rst', 'r').read(),
+    classifiers=classifiers,
+    keywords=', '.join(keywords),
+    author='Beproud',
+    packages=find_packages(),
+)

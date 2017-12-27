92a117c20d5a323e3b3e628e60fd51a48120edf6
crohaco <crohaco@beproud.jp>
2017-12-27 14:34:26 +0900

    ホストを環境変数で渡す

--- tests/postgresql/sql/__init__.py
+++ tests/postgresql/sql/__init__.py
@@ -4,5 +4,6 @@ import os
 import psycopg2
 from sql_importer import init
 
-connection = psycopg2.connect("host=db user=postgres")
+host = os.environ.get('POSTGRES_HOST', 'db')
+connection = psycopg2.connect("host={host} user=postgres".format(host=host))
 init(os.path.dirname(__file__), globals(), connection, sql_type='postgresql')
